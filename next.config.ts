1e0852795b5299a8ca2ee96db718bbe46f4a5a29
Florentin Tchangai <ftchangai@ecobank.com>
2025-03-19 13:31:33 +0000

    Layout modification and auth routes fix for production

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,29 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://sapientia-quiz-game-api.vercel.app/api/:path*",
+      },
+    ];
+  },
 };
 
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: 'https://sapientia-quiz-game-api.vercel.app/api/:path*',
-//       },
-//     ];
-//   },
-// };
+module.exports = {
+  experimental: {
+    optimizeFonts: false, // Désactive l'optimisation des polices pour voir si ça règle le problème
+    turbopack: false, // Désactive Turbopack
+  },
+  //   async rewrites() {
+  //     return [
+  //       {
+  //         source: '/api/:path*',
+  //         destination: 'https://sapientia-quiz-game-api.vercel.app/api/:path*',
+  //       },
+  //     ];
+  //   },
+};
 
 export default nextConfig;
